4f2a48d89b8283e13ee5187196d4b5224e87a670
Kevin Kern <hello@regenrek.at>
2025-08-11 21:12:27 +0200

    example/nuxt-app: migrate to @browser-echo/nuxt module; remove custom plugin and server route; configure browserEcho in nuxt.config

--- example/nuxt-app/nuxt.config.ts
+++ example/nuxt-app/nuxt.config.ts
@@ -1,17 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import browserLogsToTerminal from 'browser-echo'
-
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  vite: {
-    plugins: [
-      browserLogsToTerminal({
-        injectHtml: false,
-        enabled: true,
-        route: '/__client-logs',
-        tag: '[nuxt-browser]',
-      }),
-    ],
-  },
+  modules: ['@browser-echo/nuxt'],
+  browserEcho: {
+    enabled: true,
+    route: '/__client-logs',
+    tag: '[nuxt-browser]'
+  }
 })
